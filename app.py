2578adef1b86a9668f2331c38decd286eddea4f5
jagaimo <jagazo1214@gmail.com>
2024-06-12 20:04:31 +0900

    Initial commit

--- app.py
+++ app.py
@@ -130,6 +130,3 @@ def record_sale():
         flash(f'Googleスプレッドシートにデータを追加する際にエラーが発生しました: {error}')
 
     return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
